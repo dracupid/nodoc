5a968ec8beee1964d451e530297d331a7479910b
Zhao Jingchen <dracupid@gmail.com>
2015-01-17 21:22:24 +0800

    fix

--- example.js
+++
@@ -1,8 +0,0 @@
-doc = require('./src/index.coffee')
-fs = require('fs');
-
-doc.generate('./src/parser/index.coffee', {
-    moduleName: 'parser',
-    moduleDesc: 'This is a parser!'
-}).then (markdown)->
-    fs.writeFile('./api.md', markdown);
